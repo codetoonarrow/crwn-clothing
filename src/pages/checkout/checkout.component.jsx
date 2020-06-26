208d06a730631daece10c9969e78f9772156f27f
Luke Plourde <luke@krown.com>
2020-06-26 15:47:15 -0400

    added checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -27,8 +28,8 @@ const CheckoutPage = ({cartItems, total}) => (
             </div>
         </div>
         {
-            cartItems.map(cartItems =>
-                cartItems.name
+            cartItems.map(cartItem =>
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}> </CheckoutItem>
                 )
         }
 
