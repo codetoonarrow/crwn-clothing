87b868d89649a77a5a10dcf4e6f5874385021085
Luke Plourde <luke@krown.com>
2020-06-29 15:48:06 -0400

    Can remove items from cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types';
+import CartActionsTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
@@ -8,3 +9,8 @@ export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
 });
+
+export const clearItemFromCart = item => ({
+    type: CartActionsTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+})
