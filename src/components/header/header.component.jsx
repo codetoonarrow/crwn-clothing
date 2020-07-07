2c85f37590d16847267881ef1dc9b5cb6366ac18
Luke Plourde <luke@krown.com>
2020-07-07 15:03:09 -0400

    update the header component to styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,7 +12,7 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
     <HeaderContainer>
@@ -28,9 +28,9 @@ const Header = ({ currentUser, hidden }) => (
             </OptionLink>
             {
                 currentUser ? (
-                <OptionDiv onClick={() => auth.signOut()}>
+                <OptionLink  as='div' onClick={() => auth.signOut()}>
                     SIGN OUT
-                </OptionDiv>
+                </OptionLink>
                 ) : (
                 <OptionLink to='signin'>
                     SIGN IN
